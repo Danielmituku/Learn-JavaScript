78cc420206303aec03cf487c336d6f9a8e73b914
Daniel Mituku <dberhe490@gmail.com>
2024-03-31 18:36:28 +0300

    A new files has been added

---
+++ Objects and Functions/Script.js
@@ -0,0 +1,14 @@
+// Objects and Functions: Everything in Javascirpt is object
+
+
+//Primitives: Numbers, Strings, Booleans, Undefined,Null
+//Everything else: Arrays, Functions, objects, Dates, Wrapper for Numbers, Strings, Booleans
+
+// Object Oriented Programming: Objects interacting with one another through methods and properties,
+// Object Oriented Programming: Used to store data, structure applications into modules and keeping code clean
+
+//Constructore or Protype: is like class on other programming languages.
+//Inheritance in general: when one object is based on other object, we use protype properties
+
+
+
